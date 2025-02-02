e91fa97050004eea9b09d5a98cb4e02d665bb060
Andrew Wilson <andrew.j.wilson2479@gmail.com>
2025-02-02 17:39:23 -0500

    fix space/tab indentation

--- aac_crud_driver.py
+++ aac_crud_driver.py
@@ -33,7 +33,7 @@ class AnimalShelter(object):
 # Complete this create method to implement the C in CRUD.
     def create(self, data):
         if data is not None:
-		self.database.animals.insert_one(data)  # data should be dictionary            
+            self.database.animals.insert_one(data)  # data should be dictionary            
         else:
             raise Exception("Nothing to save, because data parameter is empty")
 
