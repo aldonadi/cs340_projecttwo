8a709ef64da08e36bc916a1d630bc5324d11af72
Andrew Wilson <andrew.j.wilson2479@gmail.com>
2025-02-02 17:23:58 -0500

    initial code from SNHU, unmodified

---
+++ aac_crud_driver.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+class AnimalShelter(object):
+    """ CRUD operations for Animal collection in MongoDB """
+
+    def __init__(self):
+        # Initializing the MongoClient. This helps to 
+        # access the MongoDB databases and collections.
+        # This is hard-wired to use the aac database, the 
+        # animals collection, and the aac user.
+        # Definitions of the connection string variables are
+        # unique to the individual Apporto environment.
+        #
+        # You must edit the connection variables below to reflect
+        # your own instance of MongoDB!
+        #
+        # Connection Variables
+        #
+        USER = 'aacuser'
+        PASS = 'SNHU1234'
+        HOST = 'nv-desktop-services.apporto.com'
+        PORT = 31580
+        DB = 'aac'
+        COL = 'animals'
+        #
+        # Initialize Connection
+        #
+        self.client = MongoClient('mongodb://%s:%s@%s:%d' % (USER,PASS,HOST,PORT))
+        self.database = self.client['%s' % (DB)]
+        self.collection = self.database['%s' % (COL)]
+
+# Complete this create method to implement the C in CRUD.
+    def create(self, data):
+        if data is not None:
+		self.database.animals.insert_one(data)  # data should be dictionary            
+        else:
+            raise Exception("Nothing to save, because data parameter is empty")
+
+# Create method to implement the R in CRUD.
+
