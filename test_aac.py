3e27fe6448e952d63a6738c8cdaf7155bc321374
Andrew Wilson <andrew.j.wilson2479@gmail.com>
2025-02-02 20:06:03 -0500

    update test script to actually print the found record(s)

--- test_aac.py
+++ test_aac.py
@@ -3,4 +3,6 @@
 from aac_crud_driver import AnimalShelter
 
 a = AnimalShelter()
-r = list(a.find( { "breed": "Labradore Retriever Mix"} ))
+r = list(a.find( { "breed": "Domestic Shorthair Mix"} ))
+
+print(r)
