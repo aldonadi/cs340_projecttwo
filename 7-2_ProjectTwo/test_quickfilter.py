af0bc8221c8929515dcc43ffd517254eafb1561d
Andrew Wilson <andrew.j.wilson2479@gmail.com>
2025-02-21 15:37:42 -0500

    add a test script for fitlers

---
+++ 7-2_ProjectTwo/test_quickfilter.py
@@ -0,0 +1,19 @@
+from quick_filter_buttons import QuickFilter, QuickFilters
+from aac_crud_driver import AnimalShelter
+import pprint
+
+filters = QuickFilters.load()
+
+print(f"Loaded {len(filters)} filters:")
+
+for filter in filters:
+    print(f"entry ({type(filter)}:")
+    print(f"  raw:  {pprint.pprint(filter)}")
+    print(f"  json: {filter.query_json()}")
+
+print("Obtaining matching records from database...", end='')
+shelter = AnimalShelter()
+matches = shelter.find(filters[0].query_json())
+
+print(f"got {len(matches)} matches")
+pprint.pprint(matches)
